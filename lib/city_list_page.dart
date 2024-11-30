1f855bf226a4c820d563687ba505da278ba2c3a1
tetsucafe <tetsucafe.go1234@gmail.com>
2024-11-30 20:27:18 +0900

    市区町村一覧のListViewを実装

--- lib/city_list_page.dart
+++ lib/city_list_page.dart
@@ -7,12 +7,39 @@ class CityListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+    const cities = [
+      '札幌市', 
+      '横浜市', 
+      '川崎市', 
+      '新潟市',
+      '大阪市',
+      '名古屋市',
+      '京都市',
+      '福岡市',
+      '岡山市',
+      '北九州市',
+      '長崎市',
+      '大分市',
+      '熊本市',
+      ]; 
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('市区町村一覧'),
       ),
-      body: const Center(
-        child: Text('市区町村の一覧画面です'),
+      body: ListView(
+        children: [
+          for (final city in cities)
+            ListTile(
+              title: Text(city),
+              subtitle: const Text('政令指定都市'),
+              trailing: const Icon(Icons.navigate_next),
+              onTap: () {
+                // TODO: 選択した市区町村の詳細画面へ遷移
+              },
+          )
+        ],
       ),
     );
   }
