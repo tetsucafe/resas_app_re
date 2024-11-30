89c14a863d937bad73b1943061787c05088d899a
tetsucafe <tetsucafe.go1234@gmail.com>
2024-11-30 19:13:00 +0900

    市区町村一覧のWigetを切り出した

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'RESAS_APP',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: CityListPage(),
+    );
+  }
+}
+
+class CityListPage extends StatelessWidget {
+  const CityListPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('市区町村一覧'),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text('RESAS_APP'),
-        ),
-        body: const Center(
-          child: Text('市区町村の一覧画面です'),
-        ),
+      body: const Center(
+        child: Text('市区町村の一覧画面です'),
       ),
     );
   }
