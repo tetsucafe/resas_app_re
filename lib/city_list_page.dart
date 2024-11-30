c4b05a086d371547527a6abca53ef63251de3c6c
tetsucafe <tetsucafe.go1234@gmail.com>
2024-12-01 00:15:39 +0900

    RESAS APIからデータを取得

--- lib/city_list_page.dart
+++ lib/city_list_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:resas_app/city_detail_page.dart';
+import 'env.dart';
+
+import 'city_detail_page.dart';
+import 'package:http/http.dart' as http;
 
 class CityListPage extends StatefulWidget {
   const CityListPage({
@@ -11,67 +14,69 @@ class CityListPage extends StatefulWidget {
 }
 
 class _CityListPageState extends State<CityListPage> {
+  late Future<String> _future;
 
-  late Future<void> _future;
-  //画面を開いた時の処理
+  // 画面を開いたときの処理
   @override
   void initState() {
     super.initState();
-    _future = Future.delayed(const Duration(seconds: 3));
+    const host = 'opendata.resas-portal.go.jp';
+    const endopoint = 'api/v1/cities';
+    final headers = {'X-API-KEY': Env.resasApiKey};
+    _future = http
+        .get(Uri.https(host, endopoint), headers: headers)
+        .then((res) => res.body);
   }
 
   @override
   Widget build(BuildContext context) {
-    
     const cities = [
-      '札幌市', 
-      '横浜市', 
-      '川崎市', 
-      '新潟市',
-      '大阪市',
-      '名古屋市',
-      '京都市',
-      '福岡市',
-      '岡山市',
+      '札幌市',
+      '横浜市',
+      '川崎市',
+      '相模原市',
+      '仙台市',
+      'さいたま市',
+      '千葉市',
+      '静岡市',
+      '浜松市',
       '北九州市',
-      '長崎市',
-      '大分市',
-      '熊本市',
-      ]; 
+    ];
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('市区町村一覧'),
       ),
-      body: FutureBuilder<void>(
-        future: _future,
-        builder: (context, snapshot) {
-          if(snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
+      body: FutureBuilder<String>(
+          future: _future,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            print(snapshot.data);
+            return ListView(
+              children: [
+                for (final city in cities)
+                  ListTile(
+                    title: Text(city),
+                    subtitle: const Text(
+                        '政令指定都市'), // This is fine as it's a constant string
+                    trailing: const Icon(Icons.navigate_next),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => CityDetailPage(
+                            city: city,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+              ],
             );
-          }
-          return ListView(
-            children: [
-              for (final city in cities)
-                ListTile(
-                  title: Text(city),
-                  subtitle: const Text('政令指定都市'),
-                  trailing: const Icon(Icons.navigate_next),
-                  onTap: () {
-                    // 選択した市区町村の詳細画面へ遷移
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => CityDetailPage(city: city),
-                      ),
-                    );
-                  },
-              )
-            ],
-          );
-        }
-      ),
+          }),
     );
   }
 }
-
